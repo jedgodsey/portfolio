442a04d2cb6cbdfaa6f3ba418d952fec26ab1c43
jedgodsey <jed.godsey@gmail.com>
2020-12-13 18:20:20 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    videoURL: 'https://assets.mixkit.co/videos/preview/mixkit-winter-storm-over-a-small-town-15500-large.mp4'
+  }
+
+  render () {
+    return (
+      <>
+        <video id="background-video" loop autoPlay>
+          <source src={this.state.videoURL} type="video/mp4" />
+          <source src={this.state.videoURL} type="video/ogg" />
+        </video>
+        <h1>IS THIS WORKING?</h1>
+      </>
+    )
+  }
+};
 
 export default App;
+
+// desert: https://assets.mixkit.co/videos/preview/mixkit-haze-across-the-desert-22146-large.mp4
+
+// koi: https://assets.mixkit.co/videos/preview/mixkit-rain-splashing-on-a-fish-pond-28101-large.mp4
+
+// rain on window: https://assets.mixkit.co/videos/preview/mixkit-window-on-a-rainy-day-2846-large.mp4
