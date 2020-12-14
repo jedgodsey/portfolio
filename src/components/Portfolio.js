9caca8225d033f50ce05b562a0090849b6b459bc
jedgodsey <jed.godsey@gmail.com>
2020-12-13 23:35:15 -0800

    working on project tiles

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,3 @@
-import React from './something/this.png';
 import walkscreen from '../assets/walk-with-me-screen.png';
 import roscoescreen from '../assets/roscoe-screen.png';
 import dryftscreen from '../assets/dryft-screen.png';
@@ -8,8 +7,32 @@ let Portfolio = () => {
   return(
     <div id="projects">
       <div className="project">
-        <div className="show">{walkscreen}</div>
-        <div className="tell"></div>
+        <div className="show"><img src={walkscreen} className="screen-shot" /></div>
+        <div className="tell">
+          <h3>walk-with-me</h3>
+          <p>"Walk With Me" is a simple online application that allows users to sign up to walk dogs at RescuePup, our fictional dog shelter.</p>
+        </div>
+      </div>
+      <div className="project">
+        <div className="show"><img src={myndexscreen} className="screen-shot" /></div>
+        <div className="tell">
+          <h3>walk-with-me</h3>
+          <p>"Walk With Me" is a simple online application that allows users to sign up to walk dogs at RescuePup, our fictional dog shelter.</p>
+        </div>
+      </div>
+      <div className="project">
+        <div className="show"><img src={dryftscreen} className="screen-shot" /></div>
+        <div className="tell">
+          <h3>walk-with-me</h3>
+          <p>"Walk With Me" is a simple online application that allows users to sign up to walk dogs at RescuePup, our fictional dog shelter.</p>
+        </div>
+      </div>
+      <div className="project">
+        <div className="show"><img src={roscoescreen} className="screen-shot" /></div>
+        <div className="tell">
+          <h3>walk-with-me</h3>
+          <p>"Walk With Me" is a simple online application that allows users to sign up to walk dogs at RescuePup, our fictional dog shelter.</p>
+        </div>
       </div>
     </div>
   )
