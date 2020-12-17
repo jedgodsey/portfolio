195c65c4cf51fc6a44c246eff555825e65f08065
jedgodsey <jed.godsey@gmail.com>
2020-12-17 11:29:02 -0800

    updated project to hooks

--- src/App.js
+++ src/App.js
@@ -1,85 +1,56 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import logo from './assets/logo.svg'
 
-class App extends Component {
-  state = {
-    videoURL: 'https://assets.mixkit.co/videos/preview/mixkit-winter-storm-over-a-small-town-15500-large.mp4',
-    view: 'about'
-  }
+const App = () => {
+  let [ vid, setVid ] = useState('https://assets.mixkit.co/videos/preview/mixkit-winter-storm-over-a-small-town-15500-large.mp4');
 
-  handleAboutClick = () => {
-    this.setState({
-      videoURL: 'https://assets.mixkit.co/videos/preview/mixkit-winter-storm-over-a-small-town-15500-large.mp4',
-      view: 'about'
-    })
-  }
-
-  handlePortfolioClick = () => {
-    this.setState({
-      videoURL: 'https://assets.mixkit.co/videos/preview/mixkit-haze-across-the-desert-22146-large.mp4',
-      view: 'portfolio'
-    })
-  }
-
-  handleContactClick = () => {
-    this.setState({
-      videoURL: 'https://assets.mixkit.co/videos/preview/mixkit-rain-splashing-on-a-fish-pond-28101-large.mp4',
-      view: 'contact'
-    })
-  }
-
-  showView = () => {
-    switch (this.state.view) {
-      case 'about':
+  let showView = () => {
+    switch (vid) {
+      case 'https://assets.mixkit.co/videos/preview/mixkit-winter-storm-over-a-small-town-15500-large.mp4':
         return (<About />)
-      case 'portfolio':
+      case 'https://assets.mixkit.co/videos/preview/mixkit-haze-across-the-desert-22146-large.mp4':
         return (<Portfolio />)
-      case 'contact':
+      case 'https://assets.mixkit.co/videos/preview/mixkit-rain-splashing-on-a-fish-pond-28101-large.mp4':
         return (<Contact />)
       default:
         return (<About />)
     }
   }
 
-
-
-  render () {
-    console.log('state: ', this.state.videoURL)
-    return (
-      <>
-        <div id="background">
-          <video key={this.state.videoURL} id="background-video" loop autoPlay muted>
-            <source src={this.state.videoURL} type="video/mp4" />
-            <source src={this.state.videoURL} type="video/ogg" />
-          </video>
-        </div>
-        <div id="content">
-          <header>
-            <div id="jed">
-              <img src={logo} alt="Jed Logo" />
-            </div>
-            <div id="options">
-              <div id="nav-background"></div>
-              <nav>
-                <div onClick={this.handleAboutClick} className="select"><h1>About Me</h1></div>
-                <div onClick={this.handlePortfolioClick} className="select"><h1>Portfolio</h1></div>
-                <div className="select"><h1><a href="https://drive.google.com/file/d/1ZN1Cvj2rtAE64ZDMZImtJz3q9WrfM4FR/view?usp=sharing" target="_blank">Experience</a></h1></div>
-                <div onClick={this.handleContactClick}className="select"><h1>Contact</h1></div>
-              </nav>
-            </div>
-          </header>
-          <main>
-            <div className="content">
-              {this.showView()}
-            </div>            
-          </main>
-        </div>
-      </>
-    )
-  }
+  return (
+    <>
+      <div id="background">
+        <video key={vid} id="background-video" loop autoPlay muted>
+          <source src={vid} type="video/mp4" />
+          <source src={vid} type="video/ogg" />
+        </video>
+      </div>
+      <div id="content">
+        <header>
+          <div id="jed">
+            <img src={logo} alt="Jed Logo" />
+          </div>
+          <div id="options">
+            <div id="nav-background"></div>
+            <nav>
+              <div onClick={() => setVid('https://assets.mixkit.co/videos/preview/mixkit-winter-storm-over-a-small-town-15500-large.mp4')} className="select"><h1>About Me</h1></div>
+              <div onClick={() => setVid('https://assets.mixkit.co/videos/preview/mixkit-haze-across-the-desert-22146-large.mp4')} className="select"><h1>Portfolio</h1></div>
+              <div className="select"><h1><a href="https://drive.google.com/file/d/1ZN1Cvj2rtAE64ZDMZImtJz3q9WrfM4FR/view?usp=sharing" target="_blank">Experience</a></h1></div>
+              <div onClick={() => setVid('https://assets.mixkit.co/videos/preview/mixkit-rain-splashing-on-a-fish-pond-28101-large.mp4')}className="select"><h1>Contact</h1></div>
+            </nav>
+          </div>
+        </header>
+        <main>
+          <div className="content">
+            {showView()}
+          </div>            
+        </main>
+      </div>
+    </>
+  )
 };
 
 export default App;
