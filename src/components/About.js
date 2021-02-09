8f1cd4437de438cb1d70f1770e1fa293d1984d4b
jedgodsey <jed.godsey@gmail.com>
2021-02-09 11:49:21 -0800

    fixed typo

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 let About = () => {
   return (
     <div className="text">
-      <h3> I take pride in being a Full-stack Software Engineer who builds things with the user in mind.   I’m an effective communicator who asks the right questions and translates complex ideas into basic and relatable concepts. In my career I’ve developed a wealth of knowledge in technologies like React, Python and databases. I hope to affect the world by using these skills to enhance the impact that other people have in their own lives... If you’ve ever wondered about who you’d want on your team when everything is going against you, I am that guy.</h3>
+      <h3> I take pride in being a Full-Stack Software Engineer who builds things with the user in mind.   I’m an effective communicator who asks the right questions and translates complex ideas into basic and relatable concepts. In my career I’ve developed a wealth of knowledge in technologies like React, Python and databases. I hope to affect the world by using these skills to enhance the impact that other people have in their own lives... If you’ve ever wondered about who you’d want on your team when everything is going against you, I am that guy.</h3>
     </div>
   )
 }
