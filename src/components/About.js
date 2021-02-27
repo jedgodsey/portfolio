eb2b3eaed395f0fc2bc293446d889682e6c478a8
jedgodsey <jed.godsey@gmail.com>
2021-02-27 12:11:44 -0800

    updated about blurb

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 let About = () => {
   return (
     <div className="text">
-      <h3> I take pride in being a Full-Stack Software Engineer who builds things with the user in mind.   I’m an effective communicator who asks the right questions and translates complex ideas into basic and relatable concepts. In my career I’ve developed a wealth of knowledge in technologies like React, Python and databases. I hope to affect the world by using these skills to enhance the impact that other people have in their own lives... If you’ve ever wondered about who you’d want on your team when everything is going against you, I am that guy.</h3>
+      <h3>I take pride in being a Software Engineer who builds across the full stack and puts the user first. I’m an effective communicator who excels at asking the right questions and translates complex ideas into basic and relatable concepts. In my career I’ve developed a wealth of knowledge in popular frontend frameworks like React, and backend technologies like Python as well as both relational and non-relational databases. I hope to affect the world by using these skills to enhance the impact that other people have in their own lives. If you’ve ever wondered who you’d want on your team when everything is going against you, I am that guy.</h3>
     </div>
   )
 }
